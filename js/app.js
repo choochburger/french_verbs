3fd5dbc6b0c591c2cd8682bf30a41758a12b9356
Chris Lyons <lyons.chr@gmail.com>
2015-02-08 22:53:07 +0100

    reveal answers

--- js/app.js
+++ js/app.js
@@ -6,14 +6,15 @@ $(function() {
     $('.btn-check').on('click', function(e) {
         e.preventDefault();
 
-        var $btn = $(this);
-        var $form = $(this).closest('form');
-        var $inputs = $form.find('input');
+        var $btn        = $(this);
+        var $form       = $(this).closest('form');
+        var $inputs     = $form.find('input');
+        var numAttempts = parseInt($form.data('attempts')) || 0;
 
         $inputs.each(function() {
             var $input = $(this);
             var $group = $input.closest('.form-group');
-            var val = $input.val();
+            var val    = $input.val();
             var answer = $input.data('answer');
 
             $group.removeClass('has-success has-error')
@@ -22,6 +23,11 @@ $(function() {
                 $group.addClass('has-success');
             } else {
                 $group.addClass('has-error');
+                $form.data('attempts', numAttempts + 1);
+
+                if (numAttempts >= 5) {
+                    $input.val(answer);
+                }
             }
         });
 
--- js/verbs.js
+++ js/verbs.js
@@ -1,4 +1,54 @@
-window.Verbs = [{
+window.Verbs = [
+
+{
+    verb: "Être",
+    tenses: [{
+        tense: "Présent",
+        conjugations: [
+            { q: "Je",         a: "suis" },
+            { q: "Tu",         a: "es" },
+            { q: "Il/Elle/On", a: "est" },
+            { q: "Nous",       a: "sommes" },
+            { q: "Vous",       a: "êtes" },
+            { q: "Ils/Elles",  a: "sont" }
+        ]
+    }, {
+        tense: "Future simple",
+        conjugations: [
+            { q: "Je",         a: "serai" },
+            { q: "Tu",         a: "seras" },
+            { q: "Il/Elle/On", a: "sera" },
+            { q: "Nous",       a: "serons" },
+            { q: "Vous",       a: "serez" },
+            { q: "Ils/Elles",  a: "seront" }
+        ]
+    }, {
+        tense: "Passé composé",
+        conjugations: [
+            { q: "Verbe",           a: "avoir" },
+            { q: "Participe Passé", a: "été" }
+        ]
+    }, {
+        tense: "Imparfait",
+        conjugations: [
+            { q: "Je",         a: "étais" },
+            { q: "Tu",         a: "étais" },
+            { q: "Il/Elle/On", a: "était" },
+            { q: "Nous",       a: "étions" },
+            { q: "Vous",       a: "étiez" },
+            { q: "Ils/Elles",  a: "étaient" }
+        ]
+    }, {
+        tense: "Impératif",
+        conjugations: [
+            { q: "Tu",         a: "sois" },
+            { q: "Nous",       a: "soyons" },
+            { q: "Vous",       a: "soyez" }
+        ]
+    }]
+},
+
+{
     verb: "Avoir",
     tenses: [{
         tense: "Présent",
@@ -44,7 +94,14 @@ window.Verbs = [{
             { q: "Vous",       a: "ayez" }
         ]
     }]
-}, {
+},
+
+
+
+
+
+
+{
     verb: "Faire",
     tenses: [{
         tense: "Present",
