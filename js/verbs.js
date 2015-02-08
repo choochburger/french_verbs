2346ab4a3dfb674cf9753da0b2d39297036126b9
Chris Lyons <lyons.chr@gmail.com>
2015-02-08 23:00:28 +0100

    more verbs

--- js/verbs.js
+++ js/verbs.js
@@ -1,117 +1,245 @@
 window.Verbs = [
-
-{
-    verb: "Être",
-    tenses: [{
-        tense: "Présent",
-        conjugations: [
-            { q: "Je",         a: "suis" },
-            { q: "Tu",         a: "es" },
-            { q: "Il/Elle/On", a: "est" },
-            { q: "Nous",       a: "sommes" },
-            { q: "Vous",       a: "êtes" },
-            { q: "Ils/Elles",  a: "sont" }
-        ]
-    }, {
-        tense: "Future simple",
-        conjugations: [
-            { q: "Je",         a: "serai" },
-            { q: "Tu",         a: "seras" },
-            { q: "Il/Elle/On", a: "sera" },
-            { q: "Nous",       a: "serons" },
-            { q: "Vous",       a: "serez" },
-            { q: "Ils/Elles",  a: "seront" }
-        ]
-    }, {
-        tense: "Passé composé",
-        conjugations: [
-            { q: "Verbe",           a: "avoir" },
-            { q: "Participe Passé", a: "été" }
-        ]
-    }, {
-        tense: "Imparfait",
-        conjugations: [
-            { q: "Je",         a: "étais" },
-            { q: "Tu",         a: "étais" },
-            { q: "Il/Elle/On", a: "était" },
-            { q: "Nous",       a: "étions" },
-            { q: "Vous",       a: "étiez" },
-            { q: "Ils/Elles",  a: "étaient" }
-        ]
-    }, {
-        tense: "Impératif",
-        conjugations: [
-            { q: "Tu",         a: "sois" },
-            { q: "Nous",       a: "soyons" },
-            { q: "Vous",       a: "soyez" }
-        ]
-    }]
-},
-
-{
-    verb: "Avoir",
-    tenses: [{
-        tense: "Présent",
-        conjugations: [
-            { q: "Je",         a: "ai" },
-            { q: "Tu",         a: "as" },
-            { q: "Il/Elle/On", a: "a" },
-            { q: "Nous",       a: "avons" },
-            { q: "Vous",       a: "avez" },
-            { q: "Ils/Elles",  a: "ont" }
-        ]
-    }, {
-        tense: "Future simple",
-        conjugations: [
-            { q: "Je",         a: "aurai" },
-            { q: "Tu",         a: "auras" },
-            { q: "Il/Elle/On", a: "aura" },
-            { q: "Nous",       a: "aurons" },
-            { q: "Vous",       a: "aurez" },
-            { q: "Ils/Elles",  a: "auront" }
-        ]
-    }, {
-        tense: "Passé composé",
-        conjugations: [
-            { q: "Verbe",           a: "avoir" },
-            { q: "Participe Passé", a: "eu" }
-        ]
-    }, {
-        tense: "Imparfait",
-        conjugations: [
-            { q: "Je",         a: "avais" },
-            { q: "Tu",         a: "avais" },
-            { q: "Il/Elle/On", a: "avait" },
-            { q: "Nous",       a: "avions" },
-            { q: "Vous",       a: "aviez" },
-            { q: "Ils/Elles",  a: "avaient" }
-        ]
-    }, {
-        tense: "Impératif",
-        conjugations: [
-            { q: "Tu",         a: "aie" },
-            { q: "Nous",       a: "ayons" },
-            { q: "Vous",       a: "ayez" }
-        ]
-    }]
-},
+    {
+        verb: "Être",
+        tenses: [{
+            tense: "Présent",
+            conjugations: [
+                { q: "Je",         a: "suis" },
+                { q: "Tu",         a: "es" },
+                { q: "Il/Elle/On", a: "est" },
+                { q: "Nous",       a: "sommes" },
+                { q: "Vous",       a: "êtes" },
+                { q: "Ils/Elles",  a: "sont" }
+            ]
+        }, {
+            tense: "Future simple",
+            conjugations: [
+                { q: "Je",         a: "serai" },
+                { q: "Tu",         a: "seras" },
+                { q: "Il/Elle/On", a: "sera" },
+                { q: "Nous",       a: "serons" },
+                { q: "Vous",       a: "serez" },
+                { q: "Ils/Elles",  a: "seront" }
+            ]
+        }, {
+            tense: "Passé composé",
+            conjugations: [
+                { q: "Verbe",           a: "avoir" },
+                { q: "Participe Passé", a: "été" }
+            ]
+        }, {
+            tense: "Imparfait",
+            conjugations: [
+                { q: "Je",         a: "étais" },
+                { q: "Tu",         a: "étais" },
+                { q: "Il/Elle/On", a: "était" },
+                { q: "Nous",       a: "étions" },
+                { q: "Vous",       a: "étiez" },
+                { q: "Ils/Elles",  a: "étaient" }
+            ]
+        }, {
+            tense: "Impératif",
+            conjugations: [
+                { q: "Tu",         a: "sois" },
+                { q: "Nous",       a: "soyons" },
+                { q: "Vous",       a: "soyez" }
+            ]
+        }]
+    },
 
 
+    {
+        verb: "Avoir",
+        tenses: [{
+            tense: "Présent",
+            conjugations: [
+                { q: "Je",         a: "ai" },
+                { q: "Tu",         a: "as" },
+                { q: "Il/Elle/On", a: "a" },
+                { q: "Nous",       a: "avons" },
+                { q: "Vous",       a: "avez" },
+                { q: "Ils/Elles",  a: "ont" }
+            ]
+        }, {
+            tense: "Future simple",
+            conjugations: [
+                { q: "Je",         a: "aurai" },
+                { q: "Tu",         a: "auras" },
+                { q: "Il/Elle/On", a: "aura" },
+                { q: "Nous",       a: "aurons" },
+                { q: "Vous",       a: "aurez" },
+                { q: "Ils/Elles",  a: "auront" }
+            ]
+        }, {
+            tense: "Passé composé",
+            conjugations: [
+                { q: "Verbe",           a: "avoir" },
+                { q: "Participe Passé", a: "eu" }
+            ]
+        }, {
+            tense: "Imparfait",
+            conjugations: [
+                { q: "Je",         a: "avais" },
+                { q: "Tu",         a: "avais" },
+                { q: "Il/Elle/On", a: "avait" },
+                { q: "Nous",       a: "avions" },
+                { q: "Vous",       a: "aviez" },
+                { q: "Ils/Elles",  a: "avaient" }
+            ]
+        }, {
+            tense: "Impératif",
+            conjugations: [
+                { q: "Tu",         a: "aie" },
+                { q: "Nous",       a: "ayons" },
+                { q: "Vous",       a: "ayez" }
+            ]
+        }]
+    },
 
 
+    {
+        verb: "Faire",
+        tenses: [{
+            tense: "Présent",
+            conjugations: [
+                { q: "Je",         a: "fais" },
+                { q: "Tu",         a: "fais" },
+                { q: "Il/Elle/On", a: "fait" },
+                { q: "Nous",       a: "faisons" },
+                { q: "Vous",       a: "faites" },
+                { q: "Ils/Elles",  a: "font" }
+            ]
+        }, {
+            tense: "Future simple",
+            conjugations: [
+                { q: "Je",         a: "ferai" },
+                { q: "Tu",         a: "feras" },
+                { q: "Il/Elle/On", a: "fera" },
+                { q: "Nous",       a: "ferons" },
+                { q: "Vous",       a: "ferez" },
+                { q: "Ils/Elles",  a: "feront" }
+            ]
+        }, {
+            tense: "Passé composé",
+            conjugations: [
+                { q: "Verbe",           a: "avoir" },
+                { q: "Participe Passé", a: "fait" }
+            ]
+        }, {
+            tense: "Imparfait",
+            conjugations: [
+                { q: "Je",         a: "faisais" },
+                { q: "Tu",         a: "faisais" },
+                { q: "Il/Elle/On", a: "faisait" },
+                { q: "Nous",       a: "faisions" },
+                { q: "Vous",       a: "faisiez" },
+                { q: "Ils/Elles",  a: "faisaient" }
+            ]
+        }, {
+            tense: "Impératif",
+            conjugations: [
+                { q: "Tu",         a: "fais" },
+                { q: "Nous",       a: "faisons" },
+                { q: "Vous",       a: "faites" }
+            ]
+        }]
+    },
 
 
-{
-    verb: "Faire",
-    tenses: [{
-        tense: "Present",
-        conjugations: [
-            { q: "Je",         a: "fais" },
-            { q: "Tu",         a: "fais" },
-            { q: "Il/Elle/On", a: "fait" },
-            { q: "Nous",       a: "faisons" },
-            { q: "Vous",       a: "faites" },
-            { q: "Ils/Elles",  a: "font" }
-        ]
-    }]
-}]
+    {
+        verb: "Dire",
+        tenses: [{
+            tense: "Présent",
+            conjugations: [
+                { q: "Je",         a: "dis" },
+                { q: "Tu",         a: "dis" },
+                { q: "Il/Elle/On", a: "dit" },
+                { q: "Nous",       a: "disons" },
+                { q: "Vous",       a: "dites" },
+                { q: "Ils/Elles",  a: "disent" }
+            ]
+        }, {
+            tense: "Future simple",
+            conjugations: [
+                { q: "Je",         a: "dirai" },
+                { q: "Tu",         a: "diras" },
+                { q: "Il/Elle/On", a: "dira" },
+                { q: "Nous",       a: "dirons" },
+                { q: "Vous",       a: "direz" },
+                { q: "Ils/Elles",  a: "diront" }
+            ]
+        }, {
+            tense: "Passé composé",
+            conjugations: [
+                { q: "Verbe",           a: "avoir" },
+                { q: "Participe Passé", a: "dit" }
+            ]
+        }, {
+            tense: "Imparfait",
+            conjugations: [
+                { q: "Je",         a: "disais" },
+                { q: "Tu",         a: "disais" },
+                { q: "Il/Elle/On", a: "disait" },
+                { q: "Nous",       a: "disions" },
+                { q: "Vous",       a: "disiez" },
+                { q: "Ils/Elles",  a: "disaient" }
+            ]
+        }, {
+            tense: "Impératif",
+            conjugations: [
+                { q: "Tu",         a: "dis" },
+                { q: "Nous",       a: "disons" },
+                { q: "Vous",       a: "dites" }
+            ]
+        }]
+    },
+
+
+    {
+        verb: "Aller",
+        tenses: [{
+            tense: "Présent",
+            conjugations: [
+                { q: "Je",         a: "vais" },
+                { q: "Tu",         a: "vas" },
+                { q: "Il/Elle/On", a: "va" },
+                { q: "Nous",       a: "allons" },
+                { q: "Vous",       a: "allez" },
+                { q: "Ils/Elles",  a: "vont" }
+            ]
+        }, {
+            tense: "Future simple",
+            conjugations: [
+                { q: "Je",         a: "irai" },
+                { q: "Tu",         a: "iras" },
+                { q: "Il/Elle/On", a: "ira" },
+                { q: "Nous",       a: "irons" },
+                { q: "Vous",       a: "irez" },
+                { q: "Ils/Elles",  a: "iront" }
+            ]
+        }, {
+            tense: "Passé composé",
+            conjugations: [
+                { q: "Verbe",           a: "être" },
+                { q: "Participe Passé", a: "allé" }
+            ]
+        }, {
+            tense: "Imparfait",
+            conjugations: [
+                { q: "Je",         a: "allais" },
+                { q: "Tu",         a: "allais" },
+                { q: "Il/Elle/On", a: "allait" },
+                { q: "Nous",       a: "allions" },
+                { q: "Vous",       a: "alliez" },
+                { q: "Ils/Elles",  a: "allaient" }
+            ]
+        }, {
+            tense: "Impératif",
+            conjugations: [
+                { q: "Tu",         a: "va" },
+                { q: "Nous",       a: "allons" },
+                { q: "Vous",       a: "allez" }
+            ]
+        }]
+    }
+]
